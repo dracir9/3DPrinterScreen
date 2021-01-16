2be80e0782f37a08f26658511080e627955a769e
dracir9 <dracir9@users.noreply.github.com>
2021-01-16 16:04:31 +0200

    Configurable heatbeds, extruder and fans number

--- src/Menu/info_w.cpp
+++ src/Menu/info_w.cpp
@@ -2,47 +2,49 @@
 #include "info_w.h"
 
 Info_W::Info_W(lcdUI* UI, tftLCD& tft):
-    Screen(UI)
+    Screen(UI), cellAdv(416.0f/(items)), cellW(cellAdv-8)
 {
     tft.fillScreen(TFT_BLACK);
     tft.setTextFont(2);
     tft.setTextSize(1);
     tft.setTextPadding(0);
-
-    tft.fillRect(0, 0, 480, 32, TFT_RED);
     tft.setTextDatum(CC_DATUM);
-    tft.setTextColor(TFT_WHITE);
-    tft.drawString("Hello world!", 240, 16);
     
     tft.setTextColor(TFT_BLACK);
-    for (uint8_t i = 0; i < tools; i++)
+    uint8_t e = 1;
+    uint8_t f = 1;
+    for (uint8_t i = 0; i < items; i++)
     {
-        tft.fillRoundRect(74 + 138*i, 40, 130, 25, 4, TFT_WHITE);
-        tft.fillRoundRect(74 + 138*i, 73, 130, 25, 4, TFT_DARKCYAN);
-        tft.fillRoundRect(74 + 138*i, 106, 130, 25, 4, TFT_DARKCYAN);
-        if(i==0)
+        tft.fillRoundRect(72 + cellAdv*i, 0, cellW, 50, 4, TFT_WHITE);
+        tft.fillRoundRect(72 + cellAdv*i, 58, cellW, 25, 4, TFT_DARKCYAN);
+        tft.fillRoundRect(72 + cellAdv*i, 91, cellW, 25, 4, TFT_DARKCYAN);
+        if (i < heatbed)
         {
-            tft.drawString("HB", 138, 52);
+            tft.drawString(String("HB") + (heatbed > 1? String(i+1): ""), 72+cellW/2+cellAdv*i, 25);
+        }
+        else if (i < tools+heatbed)
+        {
+            tft.drawString(String("E") + (tools > 1? String(e++): ""), 72+cellW/2+cellAdv*i, 25);
         }
         else
         {
-            tft.drawString("E" + String(i), 138+138*i, 52);
+            tft.drawString(String("FAN") + (fans > 1? String(f++): ""), 72+cellW/2+cellAdv*i, 25);
         }
     }
-    tft.fillRoundRect(0, 73, 66, 25, 4, TFT_NAVY);
+    tft.fillRoundRect(0, 58, 64, 25, 4, TFT_NAVY);
     tft.setTextColor(TFT_WHITE);
-    tft.drawString("Current", 33, 85);
-    tft.fillRoundRect(0, 106, 66, 25, 4, TFT_NAVY);
-    tft.drawString("Target", 33, 118);
-    tft.drawRoundRect(0, 40, 480, 92, 4, TFT_GREEN);
+    tft.drawString("Current", 32, 70);
+    tft.fillRoundRect(0, 91, 64, 25, 4, TFT_NAVY);
+    tft.drawString("Target", 32, 103);
+    tft.drawRoundRect(0, 0, 480, 117, 4, TFT_GREEN);
 
-    tft.drawRoundRect(0, 140, 480, 25, 4, TFT_GREEN);
+    tft.drawRoundRect(0, 125, 480, 25, 4, TFT_GREEN);
 
-    tft.drawString("Time:", 40, 202);
-    tft.drawString("--D --:--:--", 40, 218);
-    tft.drawString("Remaining:", 440, 202);
-    tft.drawString("--D --:--:--", 440, 218);
-    tft.drawRoundRect(0, 173, 480, 75, 4, TFT_GREEN);
+    tft.drawString("Time:", 40, 195);
+    tft.drawString("--D --:--:--", 40, 211);
+    tft.drawString("Remaining:", 440, 195);
+    tft.drawString("--D --:--:--", 440, 211);
+    tft.drawRoundRect(0, 158, 480, 90, 4, TFT_GREEN);
 
     tft.drawBmpSPIFFS("/spiffs/SDcard_64.bmp", 45, 264);
     tft.drawRoundRect(0, 256, 155, 64, 4, TFT_ORANGE);
@@ -63,23 +65,36 @@ void Info_W::render(tftLCD& tft)
     tft.setTextFont(2);
     tft.setTextSize(1);
     tft.setTextDatum(CC_DATUM);
-    tft.setTextPadding(100);
+    tft.setTextPadding(40);
 
-    // Draw update
+    // Draw temperatures
     tft.setTextColor(TFT_WHITE, TFT_DARKCYAN);
-    tft.drawString(String(random(50, 70)) + "`C", 139, 85);
-    tft.drawString(String(random(50, 70)) + "`C", 139, 118);
-    for (uint8_t i = 1; i < tools; i++)
+    for (uint8_t i = 0; i < items; i++)
     {
-        tft.drawString(String(random(180, 250)) + "`C", 139+138*i, 85);
-        tft.drawString(String(random(180, 250)) + "`C", 139+138*i, 118);
+        if (i < heatbed)
+        {
+            tft.drawString(String(random(50, 70)) + "`C", 72+cellW/2+cellAdv*i, 70);
+            tft.drawString(String(random(50, 70)) + "`C", 72+cellW/2+cellAdv*i, 103);
+        }
+        else if (i < tools+heatbed)
+        {
+            tft.drawString(String(random(180, 250)) + "`C", 72+cellW/2+cellAdv*i, 70);
+            tft.drawString(String(random(180, 250)) + "`C", 72+cellW/2+cellAdv*i, 103);
+        }
+        else
+        {
+            tft.drawString(String(random(0, 100)) + "%", 72+cellW/2+cellAdv*i, 70);
+            tft.drawString(String(random(0, 100)) + "%", 72+cellW/2+cellAdv*i, 103);
+        }
     }
+
+    // Draw positions
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    //tft.fillRoundRect(1, 101, tft.width()-2, 23, 4, TFT_BLACK);
-    tft.drawString("X: " + String(random(250)), 60, 152);
-    tft.drawString("Y: " + String(random(210)), 180, 152);
-    tft.drawString("Z: " + String(random(200)), 300, 152);
-    tft.drawString("Fr: " + String(random(150)) + "%", 420, 152);
+    tft.setTextPadding(115);
+    tft.drawString("X: " + String(random(-100, 500)), 60, 137);
+    tft.drawString("Y: " + String(random(-100, 500)), 180, 137);
+    tft.drawString("Z: " + String(random(-100, 500)), 300, 137);
+    tft.drawString("Fr: " + String(random(70, 150)) + "%", 420, 137);
     
     nextP = millis() + 1000;
 }
