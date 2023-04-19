d6b9c02de07e75a3b78da6cccbcd2e63e891f09e
dracir9 <ricard.bitria@gmail.com>
2023-04-19 20:24:22 +0200

    Add step controls

--- main/Menu/control_Scr.cpp
+++ main/Menu/control_Scr.cpp
@@ -3,7 +3,7 @@
  * @author Ricard Bitriá Ribes (https://github.com/dracir9)
  * Created Date: 10-04-2023
  * -----
- * Last Modified: 18-04-2023
+ * Last Modified: 19-04-2023
  * Modified By: Ricard Bitriá Ribes
  * -----
  * @copyright (c) 2023 Ricard Bitriá Ribes
@@ -64,6 +64,22 @@ Control_Scr::Control_Scr(lcdUI * UI, tftLCD & tft, TchScr_Drv & ts):
     // Return button
     tft.drawRoundRect(320, 270, 160, 50, 4, TFT_ORANGE);
     tft.drawBmpSPIFFS("/spiffs/return_48.bmp", 376, 277);
+
+    // Step Size
+    tft.setTextDatum(CC_DATUM);
+    tft.drawString("XY Step", 403, 172);
+    tft.drawString("Z Step", 403, 225);
+
+    tft.setTextFont(4);
+    tft.drawRoundRect(324, 164, 50, 50, 4, TFT_CYAN);
+    tft.drawString("-", 350, 190);
+    tft.drawRoundRect(430, 164, 50, 50, 4, TFT_CYAN);
+    tft.drawString("+", 456, 190);
+
+    tft.drawRoundRect(324, 217 , 50, 50, 4, TFT_CYAN);
+    tft.drawString("-", 350, 243);
+    tft.drawRoundRect(430, 217, 50, 50, 4, TFT_CYAN);
+    tft.drawString("+", 456, 243);
 
     Button tmpBut;
     tmpBut.id = 0;
@@ -161,6 +177,50 @@ Control_Scr::Control_Scr(lcdUI * UI, tftLCD & tft, TchScr_Drv & ts):
     tmpBut.enReleaseEv = true;
 
     ts.setButton(&tmpBut); // Move Z+
+
+    tmpBut.id = 9;
+    tmpBut.xmin = 324;
+    tmpBut.xmax = 374;
+    tmpBut.ymin = 164;
+    tmpBut.ymax = 214;
+    tmpBut.enPressEv = false;
+    tmpBut.enHoldEv = false;
+    tmpBut.enReleaseEv = true;
+
+    ts.setButton(&tmpBut); // Step -
+
+    tmpBut.id = 10;
+    tmpBut.xmin = 430;
+    tmpBut.xmax = 480;
+    tmpBut.ymin = 164;
+    tmpBut.ymax = 214;
+    tmpBut.enPressEv = false;
+    tmpBut.enHoldEv = false;
+    tmpBut.enReleaseEv = true;
+
+    ts.setButton(&tmpBut); // Step +
+
+    tmpBut.id = 11;
+    tmpBut.xmin = 324;
+    tmpBut.xmax = 374;
+    tmpBut.ymin = 217;
+    tmpBut.ymax = 257;
+    tmpBut.enPressEv = false;
+    tmpBut.enHoldEv = false;
+    tmpBut.enReleaseEv = true;
+
+    ts.setButton(&tmpBut); // Z Step -
+
+    tmpBut.id = 12;
+    tmpBut.xmin = 430;
+    tmpBut.xmax = 480;
+    tmpBut.ymin = 217;
+    tmpBut.ymax = 257;
+    tmpBut.enPressEv = false;
+    tmpBut.enHoldEv = false;
+    tmpBut.enReleaseEv = true;
+
+    ts.setButton(&tmpBut); // Z Step +
 }
 
 void Control_Scr::update(uint32_t deltaTime, TchScr_Drv &ts)
@@ -263,6 +323,14 @@ void Control_Scr::render(tftLCD &tft)
     tft.drawString("Y+", 403, 26);
     tft.drawString("Z-", 456, 132);
     tft.drawString("Z+", 456, 26);
+
+    tft.setTextPadding(50);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.drawString(String(stepSize, 1), 403, 190);
+    if (zStepSize < 10.0f)
+        tft.drawString(String(zStepSize, 3), 403, 243);
+    else
+        tft.drawString(String(zStepSize, 1), 403, 243);
 }
 
 void Control_Scr::handleTouch(const TchEvent& event)
@@ -300,7 +368,7 @@ void Control_Scr::handleTouch(const TchEvent& event)
             break;
 
         default:
-            break;
+            return;
         }
         _UI->requestUpdate();
     }
@@ -308,11 +376,6 @@ void Control_Scr::handleTouch(const TchEvent& event)
     {
         switch (event.id)
         {
-        case 0:
-            if (event.trigger == TrgSrc::RELEASE)
-                _UI->setScreen(lcdUI::INFO_SCR);
-            break;
-
         case 3:
             xnPressed = false;
             break;
@@ -337,6 +400,74 @@ void Control_Scr::handleTouch(const TchEvent& event)
             zpPressed = false;
             break;
 
+        default:
+            break;
+        }
+
+        switch (event.id)
+        {
+        case 0:
+            if (event.trigger == TrgSrc::RELEASE)
+                _UI->setScreen(lcdUI::INFO_SCR);
+            break;
+
+        case 9:
+            if (stepSize == 5.0f)
+                stepSize = 1.0f;
+            else if (stepSize == 10.0f)
+                stepSize = 5.0f;
+            else if (stepSize == 50.0f)
+                stepSize = 10.0f;
+            else if (stepSize == 100.0f)
+                stepSize = 50.0f;
+            break;
+
+        case 10:
+            if (stepSize == 1.0f)
+                stepSize = 5.0f;
+            else if (stepSize == 5.0f)
+                stepSize = 10.0f;
+            else if (stepSize == 10.0f)
+                stepSize = 50.0f;
+            else if (stepSize == 50.0f)
+                stepSize = 100.0f;
+            break;
+            break;
+
+        case 11:
+            if (zStepSize == 0.01f)
+                zStepSize = 0.001f;
+            else if (zStepSize == 0.1f)
+                zStepSize = 0.01f;
+            else if (zStepSize == 1.0f)
+                zStepSize = 0.1f;
+            else if (zStepSize == 5.0f)
+                zStepSize = 1.0f;
+            else if (zStepSize == 10.0f)
+                zStepSize = 5.0f;
+            else if (zStepSize == 50.0f)
+                zStepSize = 10.0f;
+            else if (zStepSize == 100.0f)
+                zStepSize = 50.0f;
+            break;
+
+        case 12:
+            if (zStepSize == 0.001f)
+                zStepSize = 0.01f;
+            else if (zStepSize == 0.01f)
+                zStepSize = 0.1f;
+            else if (zStepSize == 0.1f)
+                zStepSize = 1.0f;
+            else if (zStepSize == 1.0f)
+                zStepSize = 5.0f;
+            else if (zStepSize == 5.0f)
+                zStepSize = 10.0f;
+            else if (zStepSize == 10.0f)
+                zStepSize = 50.0f;
+            else if (zStepSize == 50.0f)
+                zStepSize = 100.0f;
+            break;
+        
         default:
             break;
         }
