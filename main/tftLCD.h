82a51b79060695336f17996bb64a986a7bdc36d3
dracir9 <ricard.bitria@gmail.com>
2023-08-26 15:42:51 +0200

    Remove unused functions from tftLCD

--- main/tftLCD.h
+++ main/tftLCD.h
@@ -7,37 +7,18 @@
 #include "Free_Fonts.h"
 #include "Vector.h"
 
-class tftSprite : public TFT_eSprite
-{
-public:
-    tftSprite(TFT_eSPI* tft): TFT_eSprite(tft){}    
-
-    void printCenter(const String &str);
-    void printCenter(const char *str);
-};
-
 class tftLCD : public TFT_eSPI
 {
 public:
-    void drawCharBg(Vec2h pos, uint8_t c, uint16_t color, uint16_t bg, uint8_t size, Vec2h *start, Vec2h dim);
-    size_t writeBg(uint8_t c, Vec2h *pos, Vec2h dim);
-    void printBg(const String &str);
-    void printBg(const String &str, Vector2<uint8_t> pad);
-    void printBg(const String &str, uint8_t pad);
-    void printBg(const String &str, Vec2h pos, Vector2<uint16_t> dim);
-    
-    void charBounds(char c, int16_t *x, int16_t *y, int16_t *minx, int16_t *miny, int16_t *maxx, int16_t *maxy);
-    Vec2h getTextBounds(const char *str);
-    Vec2h getTextBounds(const String &str);
-    Vec2h getTextBounds(const char *str, int16_t *x, int16_t *y);
-    Vec2h getTextBounds(const String &str, int16_t *x, int16_t *y);
-
-    void printCenter(const String &str);
-    void printCenter(const char *str);
-
     void drawStringWr(const char *str, int32_t x, int32_t y, uint16_t w, uint16_t h);
 
     void drawBmpSPIFFS(const char *filename, int16_t x, int16_t y);
+};
+
+class tftSprite : public tftLCD, public TFT_eSprite
+{
+public:
+    tftSprite(TFT_eSPI* tft): TFT_eSprite(tft){}    
 };
 
 #endif
